e3f612b853ee6965cb62e3c6f0b26563c88fcd58
Nicolás Marcantonio <nicolas.marcantonio@almundo.com>
2021-01-16 13:01:01 -0300

    connect to heroku postgres db

--- config/db.config.js
+++ config/db.config.js
@@ -4,6 +4,7 @@ module.exports = {
   PASSWORD: "33c317e612d5ffebfaa7d358f09ae87c64345df820558adf7a3caa7e43799a1c",
   DB: "d42ukq2pe661p7",
   dialect: "postgres",
+  ssl: true,
   pool: {
     max: 5,
     min: 0,
--- models/index.js
+++ models/index.js
@@ -5,13 +5,19 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
   operatorsAliases: '0',
-
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false 
+    }
+  },
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
     acquire: dbConfig.pool.acquire,
     idle: dbConfig.pool.idle
-  }
+  },
+
 });
 
 const db = {};
