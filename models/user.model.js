beb0e8bf16dacbfc9dbf7eda84187e706c9263af
Nicolás Marcantonio <nicolas.marcantonio@almundo.com>
2021-01-16 17:07:22 -0300

    unique email

--- models/user.model.js
+++ models/user.model.js
@@ -1,10 +1,13 @@
 module.exports = (sequelize, Sequelize) => {
   const User = sequelize.define("user", {
     email: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      unique: true,
+      allowNull: false,
     },
     password: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: false,
     }
   });
 
