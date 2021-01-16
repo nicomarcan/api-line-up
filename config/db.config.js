ff4023b3c96f40fe7359730cd141569120db320b
Nicolás Marcantonio <nicolas.marcantonio@almundo.com>
2021-01-16 12:40:03 -0300

    changed to psql

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "password",
-  DB: "lineup",
-  dialect: "mysql",
+  HOST: "ec2-34-202-5-87.compute-1.amazonaws.com",
+  USER: "sqgzueuptajgwf",
+  PASSWORD: "33c317e612d5ffebfaa7d358f09ae87c64345df820558adf7a3caa7e43799a1c",
+  DB: "d42ukq2pe661p7",
+  dialect: "postgres",
   pool: {
     max: 5,
     min: 0,
