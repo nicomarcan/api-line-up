c8196af3bebb8094cc82470f8cf92756d2c67b81
Nicolás Marcantonio <nicolas.marcantonio@almundo.com>
2021-02-17 17:17:09 -0300

    fix admin model

--- models/index.js
+++ models/index.js
@@ -27,6 +27,6 @@ db.sequelize = sequelize;
 
 db.users = require("./user.model.js")(sequelize, Sequelize);
 db.patients = require("./patient.model.js")(sequelize, Sequelize);
-db.users = require("./admin.model.js")(sequelize, Sequelize);
+db.admins = require("./admin.model.js")(sequelize, Sequelize);
 
 module.exports = db;
